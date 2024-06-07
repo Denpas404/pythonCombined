46cae8b77e5b9fdd833c72a364690581765040c0
Dennis Paaske <102860689+Denpas404@users.noreply.github.com>
2024-06-07 16:15:59 +0200

    done ?

--- Opgaver_dag2/Opgaver_dag2/e16_transform/src/transform.py
+++ Opgaver_dag2/Opgaver_dag2/e16_transform/src/transform.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 
 def transform(s1, s2):
-    return []
+    list1 = s1.split()
+    list2 = s2.split()
+
+    int_list1 = list(map(int, list1))
+    int_list2 = list(map(int, list2))
+
+    result = [a * b for a, b in zip(int_list1, int_list2)]
+
+    return result
 
 def main():
     pass
+print(transform("1 5 3", "2 6 -1"))
 
 if __name__ == "__main__":
     main()
