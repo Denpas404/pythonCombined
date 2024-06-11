3d75055cf8de043793a0a8ebad6558e7d580e71a
Dennis Paaske <102860689+Denpas404@users.noreply.github.com>
2024-06-11 10:12:42 +0200

    add coemments

--- FBI_most_wanted/src/main.py
+++ FBI_most_wanted/src/main.py
@@ -1,10 +1,14 @@
 from data_fetcher import fetch_data
 from save_to_csv import save_to_csv, update_csv_row
+import threading
+import time
+import datetime
+
 
 def main():
     while True:
         missing_person_list, gang_member_list = fetch_data()
-        
+
         print("\nPress 1 to show missing persons")
         print("Press 2 to show gang members")
         print("Press 3 to exit")
@@ -30,15 +34,22 @@ def edit_person(person_list, data_type):
     print(f"\n{data_type.capitalize()}:")
 
     if data_type == "missing_person":
-        for index, person in enumerate(person_list, start=1):
-            print(f'[{index}], Name: {person.first_name} {person.last_name}, Last Seen: {person.last_seen}\n')
-            if index > 9:
-                break
+        if len(person_list) == 0:
+            print("No missing persons found.")
+            
+        else:
+            for index, person in enumerate(person_list, start=1):
+                print(f'[{index}], Name: {person.first_name} {person.last_name}, Last Seen: {person.last_seen}\n')
+                if index > 9:
+                    break
     elif data_type == "gang_member":
-        for index, person in enumerate(person_list, start=1):
-            print(f'[{index}], Name: {person.first_name} {person.last_name}, Gang Name: {person.gang_name}\n')
-            if index > 9:
-                break
+        if len(person_list) == 0:
+            print("No gang members found, its' a peaceful world now")
+        else:
+            for index, person in enumerate(person_list, start=1):
+                print(f'[{index}], Name: {person.first_name} {person.last_name}, Gang Name: {person.gang_name}\n')
+                if index > 9:
+                    break
             
     while True:
         person_to_edit = input("Enter the number of the person you want to edit or press 0 to go back: ")
@@ -74,37 +85,36 @@ def edit_person(person_list, data_type):
         else:
             print("Invalid input. Please enter a number.")
 
-if __name__ == "__main__": 
-    main()
+def schedule_main():
+    while True:
+        now = datetime.datetime.now() # Get the current time
+        next_run = now.replace(hour=10, minute=00, second=0, microsecond=0) # Set the next run time to 10:00 AM
+
+        if now >= next_run: # If the current time is past 10:00 AM, set the next run to the next day
+            next_run += datetime.timedelta(days=1) # Add one day to the next run time
+
+        time_to_wait = (next_run - now).total_seconds() # Calculate the time to wait in seconds
+        print(f"Waiting for {time_to_wait / 60:.2f} minutes until next run at 10:00 AM")
+
+        time.sleep(time_to_wait) # Wait until the next run time
+
+        print("Running main() at 10:00 AM")
+        main_thread = threading.Thread(target=main, name='MainThread') # Create a new thread to run main
+        main_thread.start() # Start the thread
+        main_thread.join() # Wait for the thread to finish
+
+if __name__ == "__main__":
+    # Run the main function immediately
+    main_thread = threading.Thread(target=main, name='MainThread') # Create a new thread to run main
+    main_thread.start() # Start the thread
+
+    # Start the scheduling thread to run main at 5:00 AM daily
+    schedule_thread = threading.Thread(target=schedule_main, name='ScheduleThread') # Create a new thread to run the schedule_main
+    schedule_thread.start() # Start the thread
+
+    # Join the main thread to keep the program running
+    main_thread.join() # Wait for the main thread to finish
+    schedule_thread.join() # Wait for the schedule thread to finish
 
 
 
-
-# if choice == '1':
-        #     missing_person_list = fetch_data("missing_person")
-        #     print("\nMissing Persons:")
-
-        #     # Printing the list with index
-        #     for index, missing_person in enumerate(missing_person_list, start=1):
-        #         print(f'[{index}], Name: {missing_person.first_name} {missing_person.last_name}, Last Seen: {missing_person.last_seen}\n')
-
-        #     person_to_edit = input("Enter the number of the person you want to edit or press 0 to go back: ")
-        #     if person_to_edit.isdigit():
-        #         person_to_edit = int(person_to_edit)
-        #         if person_to_edit == 0:
-        #             print("\nGoing back to main menu.")
-        #             continue  # Go back to the last menu
-        #         if person_to_edit < 1 or person_to_edit > len(missing_person_list):
-        #             print("Invalid input. Going back to main menu.")
-        #             continue
-        #         else:
-        #             print("Editing person: ", missing_person_list[person_to_edit - 1].first_name, missing_person_list[person_to_edit - 1].last_name)
-        #             new_last_seen = input("Enter new last seen date: ")
-        #             # update the person in the list
-        #             missing_person_list[person_to_edit - 1].last_seen = new_last_seen
-        #             updated_person = missing_person_list[person_to_edit - 1]
-        #             update_csv_row(updated_person, "missing_person", person_to_edit)
-
-        # elif choice == '2':
-        #     gang_member_list = fetch_data("gang_member")
-        #     print("\nGang Members:")
